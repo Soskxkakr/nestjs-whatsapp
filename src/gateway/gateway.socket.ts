f59ed875e1e98f43c11b837669d55e888b30ad10
Soskxkakr <aldo.taslim@gmail.com>
2024-05-27 21:38:08 +0800

    Change: User Agent for WhatsApp

--- src/gateway/gateway.socket.ts
+++ src/gateway/gateway.socket.ts
@@ -25,6 +25,8 @@ export class Gateway implements OnModuleInit {
         authStrategy: new LocalAuth({
           dataPath: `Session-${sessionId}`,
         }),
+        userAgent:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
         webVersionCache: {
           type: 'remote',
           remotePath:
